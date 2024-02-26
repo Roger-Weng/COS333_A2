82d49b80d860c90d14cd1ec8b58995d6781a57be
VishvaIlavelan <vi6908@princeton.edu>
2024-02-25 17:44:07 -0800

    done

---
+++ dbconnect.py
@@ -0,0 +1,218 @@
+#-----------------------------------------------------------------------
+# database.py
+# Authors: Roger Weng, Vishva Ilavelan
+#-----------------------------------------------------------------------
+
+
+import sqlite3
+import contextlib
+import sys
+
+#-----------------------------------------------------------------------
+# URL for database file
+DATABASE_URL = 'file:reg.sqlite?mode=ro'
+
+# search for specific classes with certain constraints
+def search(dept_input, num_input, area_input, title_input):
+
+    try:
+        with sqlite3.connect(DATABASE_URL, isolation_level=None,
+                             uri=True) as connection:
+            with contextlib.closing(connection.cursor()) as cursor:
+                stmt_str = "SELECT classid, dept, "
+                stmt_str += "coursenum, area, title "
+                stmt_str += "FROM courses, crosslistings, classes "
+                stmt_str += "WHERE classes.courseid = courses.courseid "
+                stmt_str += "AND crosslistings.courseid "
+                stmt_str += "= courses.courseid "
+                stmt_str += "AND dept LIKE ? ESCAPE '\\' "
+                stmt_str += "AND coursenum LIKE ? ESCAPE '\\' "
+                stmt_str += "AND area LIKE ? ESCAPE '\\' "
+                stmt_str += "AND title LIKE ? ESCAPE '\\' "
+
+                # add more AND statements here for all fields
+                stmt_str += "ORDER BY dept, coursenum, classid "
+                cursor.execute(stmt_str, [f"%{dept_input}%",
+                                          f"%{num_input}%",
+                                          f"%{area_input}%",
+                                          f"%{title_input}%"])
+                table = cursor.fetchall()
+                
+                return_list = []
+                return_list.append(True)
+
+                classes_list = []
+                
+                for row in table:
+                    classid, dept, coursenum, area, title = row
+
+                    classDict = {"classid": classid, "dept": dept, 
+                                 "coursenum": coursenum, 
+                                 "area": area, "title": title}
+                    classes_list.append(classDict)
+
+                return_list.append(classes_list)
+                return return_list
+
+    except Exception as ex:
+        return_list = []
+        return_list.append(False)
+        return_list.append(ex)
+
+        print(sys.argv[0] + ":", ex, file=sys.stderr)
+  
+        return return_list
+
+
+
+
+
+def get_class_details(classid_input): 
+
+    try:
+    
+        with sqlite3.connect(DATABASE_URL, isolation_level=None,
+                             uri=True) as connection:
+            with contextlib.closing(connection.cursor()) as cursor:
+                stmt_str = "SELECT courseid, days, starttime, "
+                stmt_str += "endtime, bldg, roomnum "
+                stmt_str += "FROM classes WHERE classid = ? "
+
+                cursor.execute(stmt_str, [classid_input])
+                class_table = cursor.fetchall()
+
+                row = class_table[0]
+                course_id, days, start_time = row[0], row[1], row[2]
+                end_time, bldg, room_num = row[3], row[4], row[5]
+
+                stmt_str = "SELECT area, title, descrip, prereqs "
+                stmt_str += "FROM courses WHERE courseid = ? "
+                cursor.execute(stmt_str, [course_id])
+                course_table = cursor.fetchall()
+
+                row = course_table[0]
+                area, title = row[0], row[1]
+                descrip, prereqs = row[2], row[3]
+
+
+                stmt_str = "SELECT dept, coursenum "
+                stmt_str += "FROM crosslistings WHERE courseid = ? "
+                stmt_str += "ORDER BY dept, coursenum "
+                cursor.execute(stmt_str, [course_id])
+                dept_table = cursor.fetchall()
+
+
+                stmt_str = "SELECT profname "
+                stmt_str += "FROM coursesprofs, profs "
+                stmt_str += "WHERE courseid = ? "
+                stmt_str += "AND profs.profid = coursesprofs.profid "
+                stmt_str += "ORDER BY profname "
+                cursor.execute(stmt_str, [course_id])
+                prof_table = cursor.fetchall()
+
+                deptcoursenums_list = []
+                for row in dept_table:
+                    deptcoursenums_list.append([row[0], row[1]])
+
+                profs_list = []
+                for row in prof_table:
+                    profs_list.append(row[0])
+
+
+
+                classDict = {"courseid": course_id, "days": days, 
+                             "starttime": start_time, 
+                             "endtime": end_time, 
+                             "bldg": bldg, "rommnum": room_num, 
+                             "deptcoursenums": deptcoursenums_list, 
+                             "area": area, "title": title, "descrip": descrip, 
+                             "prereqs": prereqs, "profnames": profs_list}
+
+                
+
+                return_list = []
+                return_list.append(True)
+                return_list.append(classDict)
+
+        
+                return return_list
+
+    except Exception as ex:
+        return_list = []
+        return_list.append(False)
+        return_list.append(ex)
+
+        print(sys.argv[0] + ":", ex, file=sys.stderr)
+  
+        return return_list
+
+
+
+  
+
+# get information of the class given classid
+def get_class_table(classid_input, arg_zero):
+    try:
+        with sqlite3.connect(DATABASE_URL, isolation_level=None,
+                             uri=True) as connection:
+            with contextlib.closing(connection.cursor()) as cursor:
+                stmt_str = "SELECT courseid, days, starttime, "
+                stmt_str += "endtime, bldg, roomnum "
+                stmt_str += "FROM classes WHERE classid = ? "
+                cursor.execute(stmt_str, [classid_input])
+                table = cursor.fetchall()
+                return table
+
+    except Exception as ex:
+        print(arg_zero + ":", ex, file=sys.stderr)
+  
+
+# get information for the course given courseid
+def get_course_info(course_id, arg_zero):
+    try:
+        with sqlite3.connect(DATABASE_URL, isolation_level=None,
+                             uri=True) as connection:
+            with contextlib.closing(connection.cursor()) as cursor:
+                stmt_str = "SELECT area, title, descrip, prereqs "
+                stmt_str += "FROM courses WHERE courseid = ? "
+                cursor.execute(stmt_str, [course_id])
+                table = cursor.fetchall()
+                return table
+
+    except Exception as ex:
+        print(arg_zero + ":", ex, file=sys.stderr)
+
+# get department and course number
+def get_department_num(course_id, arg_zero):
+    try:
+        with sqlite3.connect(DATABASE_URL, isolation_level=None,
+                             uri=True) as connection:
+            with contextlib.closing(connection.cursor()) as cursor:
+                stmt_str = "SELECT dept, coursenum "
+                stmt_str += "FROM crosslistings WHERE courseid = ? "
+                stmt_str += "ORDER BY dept, coursenum "
+                cursor.execute(stmt_str, [course_id])
+                table = cursor.fetchall()
+                return table
+
+    except Exception as ex:
+        print(arg_zero + ":", ex, file=sys.stderr)
+
+
+# get professor name
+def get_profs(course_id, arg_zero):
+    try:
+        with sqlite3.connect(DATABASE_URL, isolation_level=None,
+                             uri=True) as connection:
+            with contextlib.closing(connection.cursor()) as cursor:
+                stmt_str = "SELECT profname "
+                stmt_str += "FROM coursesprofs, profs "
+                stmt_str += "WHERE courseid = ? "
+                stmt_str += "AND profs.profid = coursesprofs.profid "
+                stmt_str += "ORDER BY profname "
+                cursor.execute(stmt_str, [course_id])
+                table = cursor.fetchall()
+                return table
+
+    except Exception as ex:
+        print(arg_zero + ":", ex, file=sys.stderr)
---
+++ regserver.py
@@ -0,0 +1,91 @@
+import argparse
+import socket
+import pickle
+import os
+import sys
+import dbconnect
+
+def escape_special_characters(string):
+    return string.replace('_', '\\_').replace('%', '\\%')
+
+def handle_get_details(query_object):
+    classid = query_object.get[1]
+    return_obj = dbconnect.get_class_details(classid)
+    return_obj
+
+def handle_get_overviews(query_object):
+    
+
+    dept_name = escape_special_characters(query_object.get["dept"]
+                                          if query_object.get["dept"]
+                                          is not None else "")
+    num_value = escape_special_characters(query_object.get["coursenum"]
+                                          if query_object.get["coursenum"]
+                                          is not None else "")
+    area_name = escape_special_characters(query_object.get["area"]
+                                          if query_object.get["area"]
+                                          is not None else "")
+    title_name = escape_special_characters(query_object.get["title"]
+                                           if query_object.get["title"]
+                                           is not None else "")
+
+    return_obj = dbconnect.search(dept_name, num_value, area_name, title_name)    
+
+    return return_obj
+  
+
+
+
+def input_helper():
+    parser = argparse.ArgumentParser(
+        description="Server for the registrar application")
+    parser.add_argument('port', type=int, help="the port at which the server should listen")
+    args = parser.parse_args()
+
+    return args.port    
+    
+
+def main():
+    try:
+        port  = input_helper()
+        server_sock = socket.socket()
+        print('Opened server socket')
+        if os.name != 'nt':
+            server_sock.setsockopt(
+                socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        server_sock.bind(('', port))
+        print("Bound server socket to port")
+        server_sock.listen()
+        print("Listening")
+
+        while True:
+            try:
+                sock, _ = server_sock.accept()
+        
+                with sock:
+                    print("Accepted connection, opened socket")
+                    flo = sock.makefile(mode = 'rb')
+                    query_object = pickle.load(flo)
+                    print("Recieved command:", query_object[0])
+
+                    if query_object == 'get_overviews':
+                        return_obj = handle_get_overviews(query_object)
+
+                    if query_object == 'get_details': 
+                        return_obj = handle_get_details(query_object)
+
+                    flo = sock.makefile(mode='wb')
+                    pickle.dump(return_obj, flo)
+                    flo.flush()
+
+            except Exception as ex:
+                print(ex, file = sys.stderr)
+       
+    except Exception as ex:
+        # error in initializing server, can kill hte program
+        print(ex, file = sys.stderr)
+        sys.exit(1)
+        
+
+if __name__ == '__main__':
+    main()
