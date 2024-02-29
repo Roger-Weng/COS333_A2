147e44bbea8743acb979f63cc0d4e9edebd27189
VishvaIlavelan <vi6908@princeton.edu>
2024-02-28 19:12:07 -0800

    done

--- reg.py
+++ reg.py
@@ -1,6 +1,7 @@
 import argparse
 import sys
 import PyQt5.QtGui
+import PyQt5.QtWidgets
 
 
 
@@ -16,7 +17,8 @@ def input_helper():
 
 
 def create_labels(): 
-    dept_label = PyQt5.QtWidgets.QLabel("Dept")
+    dept_label = PyQt5.QtWidgets.QLabel("Dept:")
+    dept_label.setAlignment(PyQt5.QtCore.Qt.AlignRight | PyQt5.QtCore.Qt.AlignVCenter)
     dept_label.setAutoFillBackground(True)
     dept_palette = dept_label.palette()
     dept_palette.setColor(
@@ -24,7 +26,8 @@ def create_labels():
 
 
 
-    number_label = PyQt5.QtWidgets.QLabel("Number")
+    number_label = PyQt5.QtWidgets.QLabel("Number:")
+    number_label.setAlignment(PyQt5.QtCore.Qt.AlignRight | PyQt5.QtCore.Qt.AlignVCenter)
     number_label.setAutoFillBackground(True)
     number_palette = number_label.palette()
     number_palette.setColor(
@@ -32,7 +35,8 @@ def create_labels():
 
 
     
-    area_label = PyQt5.QtWidgets.QLabel("Area")
+    area_label = PyQt5.QtWidgets.QLabel("Area:")
+    area_label.setAlignment(PyQt5.QtCore.Qt.AlignRight | PyQt5.QtCore.Qt.AlignVCenter)
     area_label.setAutoFillBackground(True)
     area_palette = area_label.palette()
     area_palette.setColor(
@@ -40,7 +44,8 @@ def create_labels():
 
 
 
-    title_label = PyQt5.QtWidgets.QLabel("Title")
+    title_label = PyQt5.QtWidgets.QLabel("Title:")
+    title_label.setAlignment(PyQt5.QtCore.Qt.AlignRight | PyQt5.QtCore.Qt.AlignVCenter)
     title_label.setAutoFillBackground(True)
     title_palette = title_label.palette()
     title_palette.setColor(
@@ -53,29 +58,38 @@ def create_labels():
     
     
 
-
+def create_pushbutton():
+    submit_button = PyQt5.QtWidgets.QPushButton("Submit:")
+    return submit_button
     
+
+def create_list_widget():
+    listwidget = PyQt5.QtWidgets.QListWidget()
+    return listwidget
     
     
     
 def create_lineedits():
-    red_lineedit = PyQt5.QtWidgets.QLineEdit('0')
-    green_lineedit = PyQt5.QtWidgets.QLineEdit('0')
-    blue_lineedit = PyQt5.QtWidgets.QLineEdit('0')
-    return (red_lineedit, green_lineedit, blue_lineedit)
+    dept_lineedit = PyQt5.QtWidgets.QLineEdit()
+    title_lineedit = PyQt5.QtWidgets.QLineEdit()
+    area_lineedit = PyQt5.QtWidgets.QLineEdit()
+    number_lineedit = PyQt5.QtWidgets.QLineEdit()
+    return (dept_lineedit, title_lineedit, area_lineedit, number_lineedit)
   
   
-def create_control_frame(labels, lineedits):
+def create_control_frame(labels, lineedits, submitbutton):
     control_frame_layout = PyQt5.QtWidgets.QGridLayout()
     control_frame_layout.addWidget(labels[0], 0, 0)
     control_frame_layout.addWidget(labels[1], 1, 0)
     control_frame_layout.addWidget(labels[2], 2, 0)
     control_frame_layout.addWidget(labels[3], 3, 0)
 
-    control_frame_layout.addWidget(lineedits[0], 0, 0)
-    control_frame_layout.addWidget(lineedits[1], 1, 0)
-    control_frame_layout.addWidget(lineedits[2], 2, 0)
-    control_frame_layout.addWidget(lineedits[3], 3, 0)
+    control_frame_layout.addWidget(lineedits[0], 0, 1)
+    control_frame_layout.addWidget(lineedits[1], 1, 1)
+    control_frame_layout.addWidget(lineedits[2], 2, 1)
+    control_frame_layout.addWidget(lineedits[3], 3, 1)
+
+    control_frame_layout.addWidget(submitbutton, 0, 3, 4, 1)
 
     
     
@@ -83,13 +97,22 @@ def create_control_frame(labels, lineedits):
     control_frame.setLayout(control_frame_layout)
     return control_frame
 
+def create_class_list_frame(list_widget):
+    list_frame_layout = PyQt5.QtWidgets.QGridLayout()
+    list_frame_layout.setContentsMargins(0, 0, 0, 0)
+    list_frame_layout.addWidget(list_widget, 0, 0)
+    list_frame = PyQt5.QtWidgets.QFrame()
+    list_frame.setLayout(list_frame_layout)
+    return list_frame
 
 
 
-def create_central_frame(control_frame):
+def create_central_frame(control_frame, list_frame):
     central_frame_layout = PyQt5.QtWidgets.QGridLayout()
-    central_frame_layout.addWidget(control_frame, 0, 0)   
+    central_frame_layout.addWidget(control_frame, 0, 0) 
+    central_frame_layout.addWidget(list_frame, 1, 0)    
     central_frame = PyQt5.QtWidgets.QFrame()
+    central_frame_layout.setContentsMargins(0, 0, 0, 0)
     central_frame.setLayout(central_frame_layout)
     return central_frame
     
@@ -112,9 +135,13 @@ def main():
     # Create and lay out widgets.
     labels = create_labels()
     lineedits = create_lineedits()
+    submitbutton = create_pushbutton()
     
-    control_frame = create_control_frame(labels, lineedits)
-    central_frame = create_central_frame(control_frame)
+    control_frame = create_control_frame(labels, lineedits, submitbutton)
+   
+    list_widget = create_list_widget()
+    list_frame = create_class_list_frame(list_widget)
+    central_frame = create_central_frame(control_frame, list_frame)
     window = create_window(central_frame)
     
     
