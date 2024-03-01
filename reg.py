537a8d67326d4d3db1188ed2a319aa592484132f
VishvaIlavelan <vi6908@princeton.edu>
2024-02-29 16:17:28 -0800

    done

--- reg.py
+++ reg.py
@@ -2,6 +2,8 @@ import argparse
 import sys
 import PyQt5.QtGui
 import PyQt5.QtWidgets
+import socket
+import pickle
 
 
 
@@ -13,7 +15,20 @@ def input_helper():
     args = parser.parse_args()
 
     return args 
+  
+    
+    
 
+def create_pushbutton():
+    submit_button = PyQt5.QtWidgets.QPushButton("Submit:")
+    return submit_button
+    
+
+def create_list_widget():
+    listwidget = PyQt5.QtWidgets.QListWidget()
+    return listwidget
+    
+    
 
 
 def create_labels(): 
@@ -49,32 +64,18 @@ def create_labels():
     title_label.setAutoFillBackground(True)
     title_palette = title_label.palette()
     title_palette.setColor(
-        title_label.backgroundRole(),  PyQt5.QtCore.Qt.gray)
+        title_label.backgroundRole(),  PyQt5.QtCore.Qt.gray) 
 
-
-
+    
     return (dept_label, number_label, area_label, title_label)
-    
-    
-    
-
-def create_pushbutton():
-    submit_button = PyQt5.QtWidgets.QPushButton("Submit:")
-    return submit_button
-    
-
-def create_list_widget():
-    listwidget = PyQt5.QtWidgets.QListWidget()
-    return listwidget
-    
-    
+   
     
 def create_lineedits():
     dept_lineedit = PyQt5.QtWidgets.QLineEdit()
     title_lineedit = PyQt5.QtWidgets.QLineEdit()
     area_lineedit = PyQt5.QtWidgets.QLineEdit()
     number_lineedit = PyQt5.QtWidgets.QLineEdit()
-    return (dept_lineedit, title_lineedit, area_lineedit, number_lineedit)
+    return (dept_lineedit, number_lineedit, area_lineedit, title_lineedit)
   
   
 def create_control_frame(labels, lineedits, submitbutton):
@@ -113,6 +114,7 @@ def create_central_frame(control_frame, list_frame):
     central_frame_layout.addWidget(list_frame, 1, 0)    
     central_frame = PyQt5.QtWidgets.QFrame()
     central_frame_layout.setContentsMargins(0, 0, 0, 0)
+    central_frame_layout.setSpacing(0)
     central_frame.setLayout(central_frame_layout)
     return central_frame
     
@@ -127,21 +129,100 @@ def create_window(central_frame):
     return window
 
 
-def handle_line_edits(lineedits):
-    print(lineedits[0].text())
-    print(lineedits[1].text())
-    print(lineedits[2].text())
-    print(lineedits[3].text())
+def handle_form_submit(lineedits, list_widget, host, port):
 
-def handle_push_button(lineedits):
-    print(lineedits[0].text())
-    print(lineedits[1].text())
-    print(lineedits[2].text())
-    print(lineedits[3].text())
+    
 
+    dept_input  = lineedits[0].text()
+    number_input = lineedits[1].text()
+    area_input = lineedits[2].text()
+    title_input = lineedits[3].text()
+
+    communication_list = ["get_overviews", {'dept': dept_input, 
+                                            "coursenum": number_input, "area": area_input,
+                                            "title": title_input}]
+ 
+    try:
+        with socket.socket() as sock:
+            sock.connect((host, port))
+            flo = sock.makefile(mode='wb')
+            pickle.dump(communication_list, flo)
+            flo.flush()
+
+            flo = sock.makefile(mode = 'rb')
+            search_results = pickle.load(flo)
+            search_status = search_results[0]
+            results_list = search_results[1]
+
+            
+
+            if (search_status):
+                row = 0
+                list_widget.clear()
+                for class_dict in results_list:
+                    entry = '%5s %3s %4s %3s %-40s' % (class_dict["classid"], class_dict["dept"], 
+                    class_dict["coursenum"], class_dict["area"], class_dict["title"])
+                    list_widget.insertItem(row, entry)
+                    row += 1
+            else: 
+                print("error")
+
+    except Exception as ex: 
+        print(ex, file=sys.stderr)
+
+def handle_list_clicked(list_widget, host, port, window):
+    class_entry = list_widget.currentItem().text()
+    classid = int(class_entry[0:5])
+
+    communication_list = ["get_detail", classid]
+
+    try:
+        with socket.socket() as sock:
+            sock.connect((host, port))
+            flo = sock.makefile(mode='wb')
+            pickle.dump(communication_list, flo)
+            flo.flush()
+
+            flo = sock.makefile(mode = 'rb')
+            search_results = pickle.load(flo)
+            search_status = search_results[0]
+            class_dict = search_results[1]
+
+            
+
+            if (search_status):
+                display_string = "Course Id: " + str(class_dict["courseid"]) + "\n"
+                display_string += "Days: " + class_dict["days"] + "\n"
+                display_string += "Start time: " + class_dict["starttime"] + "\n"
+                display_string += "End time: " + class_dict["endtime"] + "\n"
+                display_string += "Building: " + class_dict["bldg"] + "\n"
+                display_string += "Room: " + class_dict["roomnum"] + "\n\n"
+
+                dept_nums_list = class_dict["deptcoursenums"]
+                for pair in dept_nums_list:
+                    display_string += "Dept and Number: " + pair[0] + " " + pair[1] + "\n"
+                
+                display_string += "Area: " + class_dict["area"] + "\n\n"
+                display_string += "Title: " + class_dict["title"] + "\n\n"
+                display_string += "Description: " + class_dict["descrip"] + "\n\n"
+                display_string += "Prerequisites: " + class_dict["prereqs"] + "\n\n"
+                profs = class_dict["profnames"]
+                for professor in profs:
+                    display_string += "Professor: " + professor + "\n"
+                PyQt5.QtWidgets.QMessageBox.information(
+            window, "My title", display_string)
+               
+            else: 
+                print("error")
+
+    except Exception as ex: 
+        print(ex, file=sys.stderr)
+   
 
 def main(): 
     args = input_helper() 
+    host = args.host
+    port = args.port
     
     app = PyQt5.QtWidgets.QApplication(sys.argv)
     
@@ -160,14 +241,18 @@ def main():
 
 
     def helper_line_edits():
-        handle_line_edits(lineedits)
+        handle_form_submit(lineedits, list_widget, host, port)
     for lineedit in lineedits: 
         lineedit.returnPressed.connect(helper_line_edits)
 
     def helper_push_button():
-        handle_push_button(lineedits)
+        handle_form_submit(lineedits, list_widget, host, port)
     submitbutton.clicked.connect(helper_push_button)
 
+    def helper_list_clicked():
+        handle_list_clicked(list_widget, host, port, window)
+    list_widget.itemActivated.connect(helper_list_clicked)    
+    
     
     
     window.show()
