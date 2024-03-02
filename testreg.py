f35108647db4a96cf4a37050ee739efcc00ec3a6
VishvaIlavelan <vi6908@princeton.edu>
2024-03-02 11:34:32 -0800

    done

--- testreg.py
+++ testreg.py
@@ -85,15 +85,16 @@ def main():
     host = sys.argv[1]
     port = int(sys.argv[2])
 
+    
     print(connect_server(host, port, ['get_overviews', {'dept':'COS', 'coursenum':'', 'area':'', 'title':''}]))
     print(connect_server(host, port, ['get_overviews', {'dept':'COS', 'coursenum':'2', 'area':'qr', 'title':'intro'}]))
 
     # Add more tests here.
-    
+
     # Boundary and Coverage Testing
-    
+
     # test given by class
-    
+
 
     print(connect_server(host, port, ['get_overviews', {'dept':'', 'coursenum':'', 'area':'', 'title':''}]))
     print(connect_server(host, port, ['get_overviews', {'dept':'', 'coursenum':'333', 'area':'', 'title':''}]))
@@ -107,7 +108,7 @@ def main():
     print(connect_server(host, port, ['get_overviews', {'dept':'', 'coursenum':'', 'area':'', 'title':'c\%S'}]))
     print(connect_server(host, port, ['get_overviews', {'dept':'cos', 'coursenum':'3', 'area':'', 'title':''}]))
     print(connect_server(host, port, ['get_overviews', {'dept':'', 'coursenum':'', 'area':'', 'title':'"Independent Study"'}]))
-    
+
     print(connect_server(host, port, ['get_overviews', {'dept':'', 'coursenum':'', 'area':'', 'title':'"Independent Study "'}]))
     print(connect_server(host, port, ['get_overviews', {'dept':'', 'coursenum':'', 'area':'', 'title':'"Independent Study  "'}]))
     print(connect_server(host, port, ['get_overviews', {'dept':'', 'coursenum':'', 'area':'', 'title':'" Independent Study"'}]))
@@ -118,11 +119,11 @@ def main():
 
 
 
- 
 
 
 
-    
+
+
 
     # boundary cases
 
@@ -132,9 +133,9 @@ def main():
     print(connect_server(host, port, ['get_overviews', {'dept':'', 'coursenum':'', 'area':"junk' OR 'x'='x", 'title':''}]))
     print(connect_server(host, port, ['get_overviews', {'dept':'', 'coursenum':"junk' OR 'x'='x", 'area':'', 'title':''}]))
     print(connect_server(host, port, ['get_overviews', {'dept':"junk' OR 'x'='x", 'coursenum':'', 'area':'', 'title':''}]))
-  
+
     # testing on small inputs
-    
+
 
 
 
@@ -150,20 +151,20 @@ def main():
     print(connect_server(host, port, ['get_overviews', {'dept':'', 'coursenum':'', 'area':'', 'title':''}]))
     print(connect_server(host, port, ['get_overviews', {'dept':'""', 'coursenum':'"', 'area':'', 'title':''}]))
     print(connect_server(host, port, ['get_overviews', {'dept':'', 'coursenum':'', 'area':'"', 'title':''}]))
-   
-   
+
+
 
     # testing where all flags are used with input
 
     print(connect_server(host, port, ['get_overviews', {'dept':'e', 'coursenum':'21', 'area':'sa', 'title':'the'}]))
 
-  
+
     # test each search parameter / flag individually
     print(connect_server(host, port, ['get_overviews', {'dept':'', 'coursenum':'', 'area':'', 'title':'systems'}]))
     print(connect_server(host, port, ['get_overviews', {'dept':'', 'coursenum':'', 'area':'qr', 'title':''}]))
     print(connect_server(host, port, ['get_overviews', {'dept':'phy', 'coursenum':'', 'area':'', 'title':''}]))
     print(connect_server(host, port, ['get_overviews', {'dept':'', 'coursenum':'104', 'area':'', 'title':''}]))
-   
+
 
     # test with unusual parameters
     print(connect_server(host, port, ['get_overviews', {'dept':'', 'coursenum':'', 'area':'', 'title':'-'}]))
@@ -172,15 +173,15 @@ def main():
     print(connect_server(host, port, ['get_overviews', {'dept':'', 'coursenum':'', 'area':'', 'title':'?'}]))
     print(connect_server(host, port, ['get_overviews', {'dept':'', 'coursenum':'', 'area':'', 'title':'%%%%"____-%%%%__"'}]))
     print(connect_server(host, port, ['get_overviews', {'dept':'', 'coursenum':'', 'area':'', 'title':'%_%_%_%%%%%%___%%_%__%'}]))
-    
+
     print(connect_server(host, port, ['get_overviews', {'dept':'%', 'coursenum':'_', 'area':'', 'title':''}]))
 
- 
+
 
 
     # testing multiple command lines arguments with multiple 
    #  of the same type of command line type
-  
+
 
 
     # full search
@@ -188,7 +189,7 @@ def main():
     print(connect_server(host, port, ['get_overviews', {'dept':'WWS', 'coursenum':'556b', 'area':'', 'title':'Topics in International Relations: International Justice'}]))
     print(connect_server(host, port, ['get_overviews', {'dept':'ORF', 'coursenum':'527', 'area':'', 'title':'Stochastic Calculus and Finance'}]))
     print(connect_server(host, port, ['get_overviews', {'dept':'MAT', 'coursenum':'203', 'area':'QR', 'title':'Advanced Multivariable Calculus'}]))
-  
+
 
 
     # test corner cases associated with escaping
@@ -203,17 +204,83 @@ def main():
     print(connect_server(host, port, ['get_overviews', {'dept':'', 'coursenum':'1', 'area':'', 'title':''}]))
     print(connect_server(host, port, ['get_overviews', {'dept':'', 'coursenum':'', 'area':'QR', 'title':''}]))
 
-    
+
     print(connect_server(host, port, ['get_overviews', {'dept':'c', 'coursenum':'2', 'area':'h', 'title':'h'}]))
-    
+
     # one each
     print(connect_server(host, port, ['get_overviews', {'dept':'MAT', 'coursenum':'', 'area':'', 'title':''}]))
     print(connect_server(host, port, ['get_overviews', {'dept':'', 'coursenum':'10', 'area':'', 'title':''}]))
     print(connect_server(host, port, ['get_overviews', {'dept':'', 'coursenum':'', 'area':'', 'title':'-'}]))
     print(connect_server(host, port, ['get_overviews', {'dept':'a', 'coursenum':'', 'area':'', 'title':'t'}]))
     print(connect_server(host, port, ['get_overviews', {'dept':'his', 'coursenum':'0', 'area':'', 'title':'roman'}]))
-    
-     
+
+
+
+
+
+
+
+    # test database corruption 
+
+    # drop courses table
+    corruption_helper("courses ")
+
+    # test how responds to missing table
+    print(connect_server(host, port, ['get_overviews', {'dept':'', 'coursenum':'', 'area':'QR', 'title':''}]))
+
+
+    # restore table but with a missing coloumn. 
+    restore_table("CREATE TABLE courses (courseid INTEGER DEFAULT NULL, area TEXT DEFAULT NULL, descrip TEXT DEFAULT NULL, prereqs TEXT DEFAULT NULL) ")
+
+    # test response to a missing coloumn
+    print(connect_server(host, port, ['get_overviews', {'dept':'', 'coursenum':'', 'area':'', 'title':'the'}]))
+
+
+    # test response to another dropped table 
+
+    corruption_helper("crosslistings ")
+
+    print(connect_server(host, port, ['get_overviews', {'dept':'cos', 'coursenum':'', 'area':'', 'title':''}]))
+
+    # test response with a missing coloumn
+    restore_table("CREATE TABLE crosslistings (courseid INTEGER DEFAULT NULL, coursenum TEXT DEFAULT NULL) ")
+
+    print(connect_server(host, port, ['get_overviews', {'dept':'phy', 'coursenum':'', 'area':'', 'title':''}]))
+
+
+
+    corruption_helper("classes ")
+
+    print(connect_server(host, port, ['get_overviews', {'dept':'', 'coursenum':'', 'area':'SA', 'title':''}]))
+
+
+    restore_table("CREATE TABLE classes (classid INTEGER DEFAULT NULL, courseid INTEGER DEFAULT NULL, days TEXT DEFAULT NULL, starttime TEXT DEFAULT NULL, endtime TEXT DEFAULT NULL) ")
+
+    print(connect_server(host, port, ['get_overviews', {'dept':'', 'coursenum':'', 'area':'SA', 'title':''}]))
+
+    corruption_helper("classes ")
+    corruption_helper("crosslistings ")
+
+
+    print(connect_server(host, port, ['get_overviews', {'dept':'', 'coursenum':'', 'area':'', 'title':''}]))
+
+    corruption_helper("profs ")
+    corruption_helper("coursesprofs ")
+    corruption_helper("courses ")
+
+    print(connect_server(host, port, ['get_overviews', {'dept':'', 'coursenum':'', 'area':'', 'title':''}]))
+
+
+    os.system("rm -f reg.sqlite")
+
+    print(connect_server(host, port, ['get_overviews', {'dept':'', 'coursenum':'', 'area':'', 'title':''}]))
+
+    os.system("cp /u/cos333/Asgt1Solution/reg.sqlite .")
+
+
+
+
+
 
 
        
